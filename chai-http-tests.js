3569d12a1de6a9047605fbadf4490821f0a51265
HUEN_OH <heons921@gmail.com>
2019-11-23 23:18:02 -0500

    update : chai-http-tests.js by addin records tests

--- chai-http-tests.js
+++ chai-http-tests.js
@@ -8,20 +8,9 @@ var expect = chai.expect;
 chai.use(chaiHttp);
 
 
-var postPatientData;
+var postPatientData;    // posted patient data
+var postRecordData;     // posted record data
 
-// Test GET /patients
-describe("when we issue a 'GET' to /patients'", function(){
-    it("should return HTTP 200", function(done) {
-        chai.request(URL_SERVER)
-            .get('/patients')
-            .query({}).end(function(req, res){
-                //console.log(JSON.stringify(res));
-                expect(res.status).to.equal(200);
-                done();
-            });
-    });
-});
 
 // Test POST /patients
 describe("when we issue a 'POST' to /patients'", function(){
@@ -34,6 +23,19 @@ describe("when we issue a 'POST' to /patients'", function(){
                 expect(postPatientData.first_name).to.equal('huen')
                 expect(postPatientData.last_name).to.equal('oh')
                 expect(res.status).to.equal(201);
+                done();
+            });
+    });
+});
+
+// Test GET /patients
+describe("when we issue a 'GET' to /patients'", function(){
+    it("should return HTTP 200", function(done) {
+        chai.request(URL_SERVER)
+            .get('/patients')
+            .query({}).end(function(req, res){
+                //console.log(JSON.stringify(res));
+                expect(res.status).to.equal(200);
                 done();
             });
     });
@@ -59,7 +61,7 @@ describe("when we issue a 'PUT' to /patients/:id'", function(){
             .put('/patients/' + postPatientData._id)
             .send({first_name:'huen_put', last_name:'oh_put'})
             .end(function(req, res){
-                console.log(JSON.parse(res.text))  
+                //console.log(JSON.parse(res.text))  
                 expect(JSON.parse(res.text).nModified).to.equal(1)        
                 expect(res.status).to.equal(201)
                 done();
@@ -67,13 +69,98 @@ describe("when we issue a 'PUT' to /patients/:id'", function(){
     });
 });
 
+
+
+
+
+// Record test ------------------------- //
+
+// Test GET /patients/:id/records
+describe("when we issue a 'GET' to /patients/:id/records'", function(){
+    it("should return HTTP 200", function(done) {
+        chai.request(URL_SERVER)
+            .get('/patients/' + postPatientData._id + '/records')
+            .query({}).end(function(req, res){
+                //console.log(JSON.stringify(res));
+                expect(res.status).to.equal(200);
+                done();
+            });
+    });
+});
+
+// Test POST /patients/:id/records
+describe("when we issue a 'POST' to /patients/:id/records'", function(){
+    it("should return HTTP 201", function(done) {
+        chai.request(URL_SERVER)
+            .post('/patients/' + postPatientData._id + '/records')
+            .send({patient_id: postPatientData._id
+                , nurse_name: 'nancy'
+                , date: '20190820'
+                , time: '1300'
+                , type: 'Blood Pressure'
+                , value: '100'})
+            .end(function(req, res){
+                postRecordData = JSON.parse(res.text)
+                expect(res.status).to.equal(201);
+                done();
+            });
+    });
+});
+
+
+// Test GET /records/:id
+describe("when we issue a 'GET' to /records/:id'", function(){
+    it("should return HTTP 200", function(done) {
+        chai.request(URL_SERVER)
+            .get('/records/' + postRecordData._id)
+            .end(function(req, res){
+                //console.log(JSON.parse(res.text))
+                expect(res.status).to.equal(200)
+                done();
+            });
+    });
+});
+
+// Test PUT /records/:id
+describe("when we issue a 'PUT' to /records/:id'", function(){
+    it("should return HTTP 201", function(done) {
+        chai.request(URL_SERVER)
+            .put('/records/' + postRecordData._id)
+            .send({value:'80'})
+            .end(function(req, res){
+                //console.log(JSON.parse(res.text))  
+                expect(JSON.parse(res.text).nModified).to.equal(1)        
+                expect(res.status).to.equal(201)
+                done();
+            });
+    });
+});
+
+// Test DELETE /records/:id
+describe("when we issue a 'DELETE' to /records/:id'", function(){
+    it("should return HTTP 201", function(done) {
+        chai.request(URL_SERVER)
+            .delete('/records/' + postRecordData._id)
+            .end(function(req, res){
+                //console.log(JSON.parse(res.text))   
+                expect(JSON.parse(res.text).deletedCount).to.equal(1)       
+                expect(res.status).to.equal(201)
+                done();
+            });
+    });
+});
+
+// ------------------------- Record test //
+
+
+
 // Test DELETE /patients/:id
-describe("when we issue a 'PUT' to /patients/:id'", function(){
+describe("when we issue a 'DELETE' to /patients/:id'", function(){
     it("should return HTTP 201", function(done) {
         chai.request(URL_SERVER)
             .delete('/patients/' + postPatientData._id)
             .end(function(req, res){
-                console.log(JSON.parse(res.text))   
+                //console.log(JSON.parse(res.text))   
                 expect(JSON.parse(res.text).deletedCount).to.equal(1)       
                 expect(res.status).to.equal(201)
                 done();
