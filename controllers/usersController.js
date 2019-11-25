d67c45aee0203f814b86dafbef5474f78f5a9098
HUEN_OH <heons921@gmail.com>
2019-11-25 13:21:12 -0500

    test : unable usercontrollers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+  jwt = require('jsonwebtoken');
 /*
 var mongoose = require('mongoose'),
   jwt = require('jsonwebtoken'),
