ee0610ba0676b25efb8c6437a3f3d7410ef0768c
HUEN_OH <heons921@gmail.com>
2019-12-08 01:11:50 -0500

    update : by adding datepicker

---
+++ web/js/date_picker.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    var date_input1 = $('input[name="dateOfBirth"]'); //our date input has the name "dateOfBirth"
+    var date_input2 = $('input[name="date"]'); //our date input has the name "date"
+    
+    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+    var options = {
+            format: 'yyyy/mm/dd',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+      };
+
+    date_input1.datepicker(options);
+    date_input2.datepicker(options);
+})
